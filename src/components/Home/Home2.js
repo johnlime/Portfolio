d09d1efa56065d1309383a7ffc025679680e2079
johnlime <thelimejuiceproductions@gmail.com>
2024-12-04 20:10:58 -0500

    put my name on the front page; TODO: replace images and the top left logo; about page is removed; project page is in the process of being personalized; TODO2: replace resume with mine

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -16,40 +16,11 @@ function Home2() {
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+              ABOUT ME
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple"> C++, Javascript and Go. </b>
-              </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              Software engineer and data scientist, with a background in
+              <b className="highlight"> deep reinforcement learning, evolutionary algorithms, agent-based modeling, and Bayesian statistics </b>. Interested in bringing unique approach to robotic control systems, video game AI behaviors, and simulations.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
@@ -58,58 +29,47 @@ function Home2() {
             </Tilt>
           </Col>
         </Row>
-        <Row>
-          <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
-            <p>
-              Feel free to <span className="purple">connect </span>with me
-            </p>
-            <ul className="home-about-social-links">
-              <li className="social-icons">
-                <a
-                  href="https://github.com/soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiFillGithub />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://twitter.com/Soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillInstagram />
-                </a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
       </Container>
     </Container>
   );
 }
 export default Home2;
+
+/*
+<h1 style={{ fontSize: "2.6em" }}>
+  LET ME <span className="purple"> INTRODUCE </span> MYSELF
+</h1>
+<p className="home-about-body">
+  I fell in love with programming and I have at least learnt
+  something, I think… 🤷‍♂️
+  <br />
+  <br />I am fluent in classics like
+  <i>
+    <b className="purple"> C++, Javascript and Go. </b>
+  </i>
+  <br />
+  <br />
+  My field of Interest's are building new &nbsp;
+  <i>
+    <b className="purple">Web Technologies and Products </b> and
+    also in areas related to{" "}
+    <b className="purple">
+      Blockchain.
+    </b>
+  </i>
+  <br />
+  <br />
+  Whenever possible, I also apply my passion for developing products
+  with <b className="purple">Node.js</b> and
+  <i>
+    <b className="purple">
+      {" "}
+      Modern Javascript Library and Frameworks
+    </b>
+  </i>
+  &nbsp; like
+  <i>
+    <b className="purple"> React.js and Next.js</b>
+  </i>
+</p>
+*/
